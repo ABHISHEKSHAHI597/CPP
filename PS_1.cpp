3fb65a69b1e3d37abdfe29014209e9e2223f1983
ABHISHEKSHAHI597 <shahiabhishek597@gmail.com>
2025-10-22 16:38:14 +0000

    init

--- PS_1.cpp
+++ PS_1.cpp
@@ -1,19 +1,17 @@
 // WRITE A PROGRAM TO CALCULATE THE HYPOTENUSE OF A RIGHT ANGLE TRIANGLE
 
 #include <iostream>
+using namespace std;
 #include <cmath>
 
 int main(){
     double a;
     double b;
-    std::cout<< "The 1st side is: ";
-    std::cin>> a;
-    std::cout<< "The 2nd side is: ";
-    std::cin>> b;
+    cout<< "The 1st side is: ";
+    cin>> a;
+    cout<< "The 2nd side is: ";
+    cin>> b;
     double c = sqrt(pow(a,2)+pow(b,2));
-    std::cout << "Therefore, the hypotenuse is " << c;  
-
-    
+    cout << "Therefore, the hypotenuse is " << c;
     return 0;
-    
 }
